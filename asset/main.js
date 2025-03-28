398c7fc996c3ae29eff6d5f7a513c5f1fac8606c
manhcuong <cuonglu603@gmail.com>
2025-03-28 11:43:39 +0700

    code

--- asset/main.js
+++ asset/main.js
@@ -1,4 +1,36 @@
+const list = [
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+  { id: 1, name: "DJI Phantom 2 Vision", price: 599, img: "product.png" },
+];
+loaddata();
+
 function showMenu() {
   var element = document.getElementById("menuMobile");
   element.classList.toggle("my-menu");
+}
+function loaddata() {
+  document.getElementById("listProduct").innerHTML = "";
+  list.forEach((item) => {
+    document.getElementById("listProduct").innerHTML += `
+      <div class="item-product">
+      <img src="./asset/img/${item.img}" />
+      <p>${item.name}</p>
+      <span>$ ${item.price}</span>
+    </div>
+    `;
+  });
 }
